2a1463ed509c55aff730630a96ae598f313acf24
davirrocha <davirochamoraes7@gmail.com>
2024-03-27 22:31:15 -0300

    modifi-convertFrom

--- document.js
+++ document.js
@@ -1,20 +1,22 @@
 const convertButton = document.querySelector(".button-convert")
 
 function diceConvert() {
-    const inputCurrency = parseFloat(document.querySelector(".input-value").value);
+    const inputCurrency = document.querySelector(".input-value").value;
     const valueConvertion = document.querySelector("#real")
     const valueConvertionDolar = document.querySelector("#dolar")
     const realToday = 0.20
     const dolarToday = 4.97
-    const euroToday = 5.39
+    const euroToday =  5.3928
     const libraToday = 6.36
     const yenToday = 0.034
     const bitcoin = 3
+    const todayDolar = 0.92
     const todayEuro = 1.08
     const covertFrom = document.querySelector(".convert-from")
-    const valueInput = document.querySelector(".convert-to")
+    const convertTo = document.querySelector(".convert-to")
     const valueTo = document.querySelector(".covert-from")
     const imgValue = document.querySelector(".eua")
+
 
     valueConvertion.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
@@ -22,47 +24,80 @@ function diceConvert() {
     }).format(inputCurrency)
 
 
-    if (valueInput.value == "real") {
+    if (convertTo.value == "real") {
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL",
+
         }).format(inputCurrency / realToday)
     }
 
-    if (valueInput.value == "dolar") {
+    if (convertTo.value == "dolar") {
         //Se o select estiver selecionado o valor de dolar, entre aqui
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("en-IN", {
             style: "currency",
             currency: "USD",
-        }).format(inputCurrency / dolarToday) //NÃO PODE DAR ESPAÇO
+        }).format(inputCurrency / dolarToday) //NÃO PODE DEIXAR JUNTOS
     }
 
-    if (valueInput.value == "euro") {
+    if (convertTo.value == "euro") {
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("nl-BE", {
             style: "currency",
             currency: "EUR",
         }).format(inputCurrency / euroToday)
     }
 
-    if (valueInput.value == "libra") {
+    if (convertTo.value == "libra") {
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "GBP",
         }).format(inputCurrency / libraToday)
     }
 
-    if (valueInput.value == "bitcoin") {
+    if (convertTo.value == "bitcoin") {
         valueConvertionDolar.innerHTML = new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BTC",
         }).format(inputCurrency * bitcoin)
     }
 
-    if (valueInput.value == "yen") {
-        valueConvertionDolar.innerHTML = new Intl.NumberFormat("ja-JP", {
+    if (convertTo.value == "yen") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("ja-JP", {
             style: "currency",
             currency: "JPY",
         }).format(inputCurrency / yenToday)
+    }
+
+    else if (covertFrom.value == "dolar") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("en-IN", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputCurrency * todayDolar)
+    }
+    
+    else if (covertFrom.value == "euro") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("nl-BE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputCurrency / todayEuro)
+    }
+    else if (covertFrom.value == "libra") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "GBP",
+        }).format(inputCurrency * todayEuro)
+
+    } else if (covertFrom.value == "bitcoin") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BTC",
+        }).format(inputCurrency * todayEuro)
+
+    } else if (covertFrom.value == "yen") {
+        valueConvertion.innerHTML = new Intl.NumberFormat("ja-Jp", {
+            style: "currency",
+            currency: "JPY",
+        }).format(inputCurrency * todayEuro)
     }
 }
 
@@ -157,7 +192,7 @@ covertFrom.addEventListener("change", function () {
         currencyImg.src = "./assets/yen.png"
 
     }
-    coinSelector()
+    diceConvert()
 })
 
 
